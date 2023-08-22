a9144aa6d3307a8fa18acd40c485cf3500de58e6
xingqilv <2014178096@qq.com>
2023-08-22 19:06:56 +0800

    今天的代码

---
+++ src/com/lzlgdx/day0822/FileDemo.java
@@ -0,0 +1,7 @@
+package com.lzlgdx.day0822;/**
+ * @author 吕星琪
+ * @version 1.10 2023/8/22 上午 09:39
+ * @since jdk1.0
+ */
+public class FileDemo {
+}
---
+++ src/com/lzlgdx/day0822/RandomAccessFileDemo1.java
@@ -0,0 +1,7 @@
+package com.lzlgdx.day0822;/**
+ * @author 吕星琪
+ * @version 1.10 2023/8/22 下午 05:01
+ * @since jdk1.0
+ */
+public class RandomAccessFileDemo1 {
+}
